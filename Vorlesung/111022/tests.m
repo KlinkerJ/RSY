f10f39c368253f656611a10bab19554747e1611f
Jonas Klinker <jonas@klindev.de>
2022-10-11 11:27:24 +0200

    Setup Theta1 according to Kinematics of a UR5, Rasmus Skovgaard Andersen
    Co-authored-by: Lennart Fuhrig <lennart2810@users.noreply.github.com>
    Co-authored-by: MatthiasUesbeck <MatthiasUesbeck@users.noreply.github.com>
    Co-authored-by: DominikKuesters <DominikKuesters@users.noreply.github.com>
    Co-authored-by: MH98 <MH98@users.noreply.github.com>
    Co-authored-by: jenslu <jenslu@users.noreply.github.com>

--- Vorlesung/111022/tests.m
+++ Vorlesung/111022/tests.m
@@ -1,4 +1,4 @@
-%We need 3 vectors for ikSolverUR3()
+%We need 3 vectors for ikSolverUR3() as well as for our ik_matlab
 %The input pos is a 3x1 vector with x-, y-, z-coordinate for the desired position of the end-effector.
 %The input eul is a 3x1 vector with the desired orientation of the end-effector in euler angles (Z,Y,X).
 %The input q is the previous configuration (joint angles) of the robot (6x1 vector).
@@ -7,4 +7,5 @@ a = [1,1,1]; % cartesion positions for endeffector in mm
 b = [pi, pi, pi]; % orientation (euler) for endeffector in rad 
 c = [pi, pi, pi, pi, pi, pi]; % joint angles in rad
 
-ik_matlab_beispiel(a,b,c)
+%ik_matlab_beispiel(a,b,c) %WARNING: This script (pulled via moodle) is using UR3 parameters, our robot is an UR5e
+ik_matlab(a, b, c)
