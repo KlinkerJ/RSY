6d83f6fed444672ae494105ac3ef08b2110bf19f
KlinkerJ <jonasklinker@gmail.com>
2022-10-24 21:15:13 +0200

    Fixed tests.m, changed filenames, syntax fixes

--- Vorlesung/111022/tests.m
+++ Vorlesung/111022/tests.m
@@ -3,9 +3,9 @@
 %The input eul is a 3x1 vector with the desired orientation of the end-effector in euler angles (Z,Y,X).
 %The input q is the previous configuration (joint angles) of the robot (6x1 vector).
 
-a = [1,1,1]; % cartesion positions for endeffector in mm
-b = [pi, pi, pi]; % orientation (euler) for endeffector in rad 
+a = [1, 1, 1]; % cartesion positions for endeffector in mm
+b = [pi, pi, pi]; % orientation (euler) for endeffector in rad
 c = [pi, pi, pi, pi, pi, pi]; % joint angles in rad
 
-%ik_matlab_beispiel(a,b,c) %WARNING: This script (pulled via moodle) is using UR3 parameters, our robot is an UR5e
-ik_matlab(a, b, c)
+%ik_matlab_beispiel(a, b, c) %WARNING: This script (pulled via moodle) is using UR3 parameters, our robot is an UR5e
+ik_matlab_ur5e(a, b, c)
