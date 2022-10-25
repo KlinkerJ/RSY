a29d56027d88e8793bd250378ee469945c2d4407
KlinkerJ <jonasklinker@gmail.com>
2022-10-25 11:34:36 +0200

    Checks for comparison of forward and inverted kinematic

--- Vorlesung/111022/fk_matlab_ur.m
+++ Vorlesung/111022/fk_matlab_ur.m
@@ -1,9 +1,4 @@
-function pos, eul = fk_matlab_ur5e(thetaArr)
-    
-    load_DH_matrices;
-    load_constants_UR5E;
-
-    %(theta1, theta2, theta3, theta4, theta5, theta6) = thetaArr;
+function [pos, eul] = fk_matlab_ur(thetaArr, DHall)
     theta1 = thetaArr(1);
     theta2 = thetaArr(2);
     theta3 = thetaArr(3);
@@ -11,10 +6,10 @@ function pos, eul = fk_matlab_ur5e(thetaArr)
     theta5 = thetaArr(5);
     theta6 = thetaArr(6);
     
-    
     pos_matrix = subs(DHall);
     pos = pos_matrix(1:3,4);
     pos = double(pos);
     eul = pos_matrix(1:3,1:3);
-    eul = doule(eul);
+    eul = diag(double(eul));
+
 end
