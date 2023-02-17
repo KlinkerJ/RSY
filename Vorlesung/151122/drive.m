77615e26ba4b4d6db9b20475803a451e0af5d191
KlinkerJ <jonasklinker@gmail.com>
2023-02-17 16:15:44 +0100

    Deleted files as they are transferred to archive already

--- Vorlesung/151122/drive.m
+++
@@ -1,5 +0,0 @@
-function drive(pos, eul, qPre, alphaArr, a, d)
-    global BewWinkel
-    BewWinkel = ik_matlab_ur(pos, eul, qPre, alphaArr, a, d);
-    moveIt
-end
--- Vorlesung/151122/drive_copy.m
+++
@@ -1,5 +0,0 @@
-function drive_copy(pos, eul, qPre, alphaArr, a, d)
-    global BewWinkel
-    BewWinkel = ik_matlab_ur(pos, eul, qPre, alphaArr, a, d);
-    moveIt_copy
-end
